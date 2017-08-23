e339ffeebd4ba680fa57f9446a88c87fd18091fe
刘占峰 <18272734568@163.com>
2017-08-23 10:54:15 +0800

    Initial Commit

---
+++ Smartbudget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Smartbudget
+//
+//  Created by Daisy on 2017/8/23.
+//  Copyright © 2017年 zf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
