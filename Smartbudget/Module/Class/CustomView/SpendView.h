2fa0e4f167cdd629b7634bef8519d9e9840b13a7
zhanfeng <18272734568@163.com>
2017-09-06 20:18:38 +0800

    update

---
+++ Smartbudget/Module/Class/CustomView/SpendView.h
@@ -0,0 +1,13 @@
+//
+//  SpendView.h
+//  Smartbudget
+//
+//  Created by zhanfeng on 2017/9/6.
+//  Copyright © 2017年 zf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpendView : UIView
+
+@end
---
+++ Smartbudget/Module/Class/CustomView/SpendView.m
@@ -0,0 +1,27 @@
+//
+//  SpendView.m
+//  Smartbudget
+//
+//  Created by zhanfeng on 2017/9/6.
+//  Copyright © 2017年 zf. All rights reserved.
+//
+
+#import "SpendView.h"
+
+@interface SpendView()
+
+@end
+
+@implementation SpendView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+
+@end
